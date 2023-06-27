1202069034f3ba96551cc0b5d9643c546a4602ed
tomekz <zadrozny.tomasz@gmail.com>
2023-06-27 12:29:26 +0200

    configured NAT gateway to have internet access from private network

--- lab-kafka/terraform/network.tf
+++ lab-kafka/terraform/network.tf
@@ -14,6 +14,17 @@ data "aws_availability_zones" "azs" {
   state = "available"
 }
 
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public_1.id
+  tags          = local.tags
+}
+
+resource "aws_eip" "nat" {
+  vpc = true
+  tags = local.tags
+}
+
 resource "aws_subnet" "public_1" {
   vpc_id            = aws_vpc.main.id
   map_public_ip_on_launch = true
@@ -40,6 +51,21 @@ resource "aws_route_table" "internet_route" {
     ignore_changes = all
   }
   tags = local.tags
+}
+
+# Update the route table for the private subnet to use the NAT gateway
+resource "aws_route_table" "private_route" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+}
+
+resource "aws_route_table_association" "private_association" {
+  subnet_id      = aws_subnet.private_1.id
+  route_table_id = aws_route_table.private_route.id
 }
 
 resource "aws_route_table_association" "set-master-default-rt-assoc" {
