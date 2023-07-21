99e239b2592348509b4a55a6397b27c4a86aba9e
tomekz <zadrozny.tomasz@gmail.com>
2023-07-21 10:21:09 +0200

    provision ecr

--- lab-kafka/terraform/ecr.tf
+++ lab-kafka/terraform/ecr.tf
@@ -1,7 +1,7 @@
-# resource "aws_ecr_repository" "my_ecr_repository" {
-#   name                 = "lab-kafka-ecr"
-#   image_tag_mutability = "MUTABLE"
-#   tags = {
-#     Name = "lab-kafka-ecr"
-#   }
-# }
+resource "aws_ecr_repository" "my_ecr_repository" {
+  name                 = "lab-kafka-ecr"
+  image_tag_mutability = "MUTABLE"
+  tags = {
+    Name = "lab-kafka-ecr"
+  }
+}
