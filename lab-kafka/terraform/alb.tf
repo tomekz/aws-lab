60d96b0d6d3606498c8de1cf2ec1abeb00ee2003
tomekz <zadrozny.tomasz@gmail.com>
2023-07-21 11:02:42 +0200

    setup aws ecs

---
+++ lab-kafka/terraform/alb.tf
@@ -0,0 +1,60 @@
+resource "aws_alb" "application_load_balancer" {
+  name               = "lab-kafka-alb"
+  internal           = false
+  load_balancer_type = "application"
+  subnets            = [ aws_subnet.public_1.id ]
+  security_groups    = [aws_security_group.load_balancer_security_group.id]
+  tags = merge(local.tags, { "Name" = "lab-kafka-alb" })
+}
+
+resource "aws_security_group" "load_balancer_security_group" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  tags = merge(local.tags, { "Name" = "lab-kafka-alb-sg" })
+}
+
+resource "aws_lb_target_group" "target_group" {
+  name        = "lab-kafka-alb-tg"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = aws_vpc.main.id
+
+  health_check {
+    healthy_threshold   = "3"
+    interval            = "300"
+    protocol            = "HTTP"
+    matcher             = "200"
+    timeout             = "3"
+    path                = "/"
+    unhealthy_threshold = "2"
+  }
+
+  tags = merge(local.tags, { "Name" = "lab-kafka-alb-tg" })
+}
+
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_alb.application_load_balancer.id
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target_group.id
+  }
+}
