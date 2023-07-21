1bfdc291cccc09cc91a6dc37dacb7adb48387997
tomekz <zadrozny.tomasz@gmail.com>
2023-07-21 11:48:22 +0200

    remove ecs service definition for now

--- lab-kafka/terraform/ecs_service.tf
+++ lab-kafka/terraform/ecs_service.tf
@@ -41,48 +41,48 @@ resource "aws_security_group" "service_security_group" {
   tags = merge(local.tags, { "Name" = "lab-kafka-lb-sg" })
 }
 
-resource "aws_ecs_service" "lab-kafka-ecs-service" {
-  name            = "lab-kafka-ecs-service"
-  cluster         = aws_ecs_cluster.lab_kafka_ecs_cluster.id
-  # launch_type = "FARGATE" # TODO - find out why this is unsupported
-  scheduling_strategy  = "REPLICA"
-  desired_count        = 1
-  force_new_deployment = true
-  tags = merge(local.tags, { "Name" = "lab-kafka-ecs-service" })
-  task_definition = aws_ecs_task_definition.lab_kafka_service_task_definition.arn
-  network_configuration {
-    subnets = [aws_subnet.private_1.id]
-    assign_public_ip = false
-    security_groups = [
-      aws_security_group.service_security_group.id,
-      aws_security_group.load_balancer_security_group.id
-    ]
-  }
+# resource "aws_ecs_service" "lab-kafka-ecs-service" {
+#   name            = "lab-kafka-ecs-service"
+#   cluster         = aws_ecs_cluster.lab_kafka_ecs_cluster.id
+#   # launch_type = "FARGATE" # TODO - find out why this is unsupported
+#   scheduling_strategy  = "REPLICA"
+#   desired_count        = 1
+#   force_new_deployment = true
+#   tags = merge(local.tags, { "Name" = "lab-kafka-ecs-service" })
+#   task_definition = aws_ecs_task_definition.lab_kafka_service_task_definition.arn
+#   network_configuration {
+#     subnets = [aws_subnet.private_1.id]
+#     assign_public_ip = false
+#     security_groups = [
+#       aws_security_group.service_security_group.id,
+#       aws_security_group.load_balancer_security_group.id
+#     ]
+#   }
+#
+#   load_balancer {
+#     target_group_arn = aws_lb_target_group.target_group.arn
+#     container_name   = "lab-kafka-esc-service-container"
+#     container_port   = 8080
+#   }
+# }
 
-  load_balancer {
-    target_group_arn = aws_lb_target_group.target_group.arn
-    container_name   = "lab-kafka-esc-service-container"
-    container_port   = 8080
-  }
-}
-
-resource "aws_ecs_task_definition" "lab_kafka_service_task_definition" {
-  family                   = "my-task-definition"
-  container_definitions    = <<TASK_DEF
-  [
-    {
-      "name": "my-container",
-      "image": "redis",
-      "cpu": 256,
-      "memory": 512,
-      "portMappings": [
-        {
-          "containerPort": 80,
-          "hostPort": 0,
-          "protocol": "tcp"
-        }
-      ]
-    }
-  ]
-  TASK_DEF
-}
+# resource "aws_ecs_task_definition" "lab_kafka_service_task_definition" {
+#   family                   = "my-task-definition"
+#   container_definitions    = <<TASK_DEF
+#   [
+#     {
+#       "name": "my-container",
+#       "image": "redis",
+#       "cpu": 256,
+#       "memory": 512,
+#       "portMappings": [
+#         {
+#           "containerPort": 80,
+#           "hostPort": 0,
+#           "protocol": "tcp"
+#         }
+#       ]
+#     }
+#   ]
+#   TASK_DEF
+# }
