cdd027a999d102e08ea6c97a290e1ddeac326e5b
tomekz <zadrozny.tomasz@gmail.com>
2023-09-18 20:45:48 +0200

    add script to write kubeconfig for Amazon EKS

--- eks/make.sh
+++ eks/make.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+CLUSTER_NAME=lab
+AWS_REGION=eu-central-1
+
 cluster-create() {
     eksctl create cluster  --config-file cluster.yaml 
 }
@@ -8,10 +11,16 @@ cluster-delete() {
     eksctl delete cluster  --config-file cluster.yaml  --wait
 }
 
+write-kubeconfig() {
+    aws eks --region $AWS_REGION update-kubeconfig --name $CLUSTER_NAME
+}
+
 if [ "$1" == "cluster-create" ]; then
     cluster-create
 elif [ "$1" == "cluster-delete" ]; then
     cluster-delete
+elif [ "$1" == "write-kubeconfig" ]; then
+    write-kubeconfig
 else
-    echo "Usage: $0 {cluster-create|cluster-delete}"
+    echo "Usage: $0 {cluster-create|cluster-delete|write-kubeconfig}"
 fi
